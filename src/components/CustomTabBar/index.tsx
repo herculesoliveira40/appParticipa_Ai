87f719a73212453ecdc73fc493e86219d9268e8a
herculesoliveira40 <herculesoliveira40@gmail.com>
2024-11-22 00:26:41 -0300

    tabs icon

--- src/components/CustomTabBar/index.tsx
+++ src/components/CustomTabBar/index.tsx
@@ -12,26 +12,26 @@ export default ({ state, navigation }: any)=>{
     return (
        <View style={style.tabArea}>
             <TouchableOpacity style={style.tabItem} onPress={()=>go('Login')}>
-                <AntDesign
-                    name="bars"
+                <MaterialIcons
+                    name="insights"
                     style={{opacity:state.index===0?1:0.5,color:themas.Colors.primary,fontSize:32}}
                 />
             </TouchableOpacity>                
             <TouchableOpacity style={style.tabItem} onPress={()=>go('Dashboard')}>
-                <AntDesign
-                    name="bars"
+                <MaterialIcons
+                    name="content-paste"
                     style={{opacity:state.index===1?1:0.5,color:themas.Colors.primary,fontSize:32}}
                 />
             </TouchableOpacity>                
             <TouchableOpacity style={style.tabItem} onPress={()=>go('Educ')}>
-                <AntDesign
-                    name="bars"
+                <MaterialIcons
+                    name="info-outline"
                     style={{opacity:state.index===2?1:0.5,color:themas.Colors.primary,fontSize:32}}
                 />
             </TouchableOpacity>                
             <TouchableOpacity style={style.tabItem} onPress={()=>go('User')}>
-                <AntDesign
-                    name="bars"
+                <MaterialIcons
+                    name="person-outline"
                     style={{opacity:state.index===3?1:0.5,color:themas.Colors.primary,fontSize:32}}
                 />
             </TouchableOpacity>                
@@ -41,4 +41,4 @@ export default ({ state, navigation }: any)=>{
 }
 
 
-//40 https://www.youtube.com/watch?v=1iYA1Qf_2UI&list=PLN5FV-HmjCA8UKWLep7O31PtQYqML8-Wd&index=2&ab_channel=CaioeduardoDev 
+//0 https://www.youtube.com/watch?v=3qZxSw_TIr4&list=PLN5FV-HmjCA8UKWLep7O31PtQYqML8-Wd&index=4&ab_channel=CaioeduardoDev
